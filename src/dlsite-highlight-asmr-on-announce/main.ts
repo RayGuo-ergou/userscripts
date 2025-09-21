ebb6b45561d2e42ede929d88da5134d791d3ea7f
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2025-09-21 23:28:32 +1000

    feat: dlsite inject css not run js

--- src/dlsite-highlight-asmr-on-announce/main.ts
+++ src/dlsite-highlight-asmr-on-announce/main.ts
@@ -5,23 +5,17 @@
  * As I only purchase ASMR items on DLSite, I want to highlight them on the announce page.
  * And DLSite does not provide a filter for ASMR items, so I have to do it myself.
  */
-import { findElementsByXPath } from '../utility/document'
+const Css = String.raw
 
-const xpath = '//*[contains(text(), \'ボイス・ASMR\')]'
-// Find elements using XPath
-const elements = findElementsByXPath(xpath)
+const borderStyle = Css`
+.n_worklist_item:has(.work_category.type_SOU){
+  border: 2px solid #86198f !important;
+  border-radius: 0.25rem !important;
+}
+`
+const styleTag = document.createElement('style')
+styleTag.textContent = borderStyle
 
-// Loop through these elements
-elements.forEach((el) => {
-  // Traverse up the DOM tree to find the parent with class 'n_worklist_item'
-  let parent = el.parentElement
-  while (parent && !parent.classList.contains('n_worklist_item')) {
-    parent = parent.parentElement
-  }
+styleTag.id = 'dlsite-highlight-asmr-on-announce'
 
-  // If a matching parent is found, add a border to it
-  if (parent) {
-    parent.style.border = '2px solid #86198f' // Customize the border style here
-    parent.style.borderRadius = '0.25rem'
-  }
-})
+document.head.appendChild(styleTag)
