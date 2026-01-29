e0376dfc2eb09c4153afe1c35c2c6da7b29e8851
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2026-01-29 19:08:47 +1100

    feat: remove bilibili removve ad

--- src/bilibili-remove-ad/config.ts
+++
@@ -1,8 +0,0 @@
-const config: Partial<Tampermonkey.ScriptMetadata> = {
-  name: 'Remove Bilibili Ads',
-  description: 'Remove ad feed cards from Bilibili pages',
-  version: '0.0.1',
-  matches: ['https://www.bilibili.com/*'],
-}
-
-export default config
